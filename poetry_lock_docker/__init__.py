f8e555c92c85572cedc6d9d590929e45f4826be0
Bram Neijt <bram@neijt.nl>
2021-04-16 22:32:30 +0200

    Use pyramids
    Becaues, why not.

--- poetry_lock_docker/__init__.py
+++ poetry_lock_docker/__init__.py
@@ -1,11 +1,15 @@
-import cherrypy
+from wsgiref.simple_server import make_server
+from pyramid.config import Configurator
+from pyramid.response import Response
 
 
-class HelloWorld(object):
-    @cherrypy.expose
-    def index(self):
-        return "Hello World!"
+def hello_world(request):
+    return Response("Hello docker container!")
 
 
-cherrypy.server.socket_host = "0.0.0.0"
-cherrypy.quickstart(HelloWorld())
+with Configurator() as config:
+    config.add_route("hello", "/")
+    config.add_view(hello_world, route_name="hello")
+    app = config.make_wsgi_app()
+server = make_server("0.0.0.0", 8080, app)
+server.serve_forever()
