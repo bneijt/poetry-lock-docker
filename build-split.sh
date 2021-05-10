28cb524efb5325831f22e2511fcb39caabab1ab1
Bram Neijt <bram@neijt.nl>
2021-05-10 19:15:51 +0200

    Add example of split builds: environment and package in separate layers

---
+++ build-split.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+set -e
+poetry build
+poetry run poetry-lock-package --build --no-parent
+
+if [ -z "$PIP_INDEX_URL" ];
+then
+    echo "You should set and authenticated PIP_INDEX_URL if you use a private repository";
+fi
+export DOCKER_BUILDKIT=1
+docker build \
+    --secret id=pip_index_url,env=PIP_INDEX_URL \
+    -f Dockerfile.split \
+    .
+echo "Now you can run that hash using"
+echo "docker run -p 8080:8080 -it <hash reported by build>"
